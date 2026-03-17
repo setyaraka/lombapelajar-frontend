21a05aad064904b9430ff47709fd76bf5a705650
setyaraka <setyarakap@gmail.com>
2026-03-17 22:45:31 +0700

    update vite api url

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,14 +1,3 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useAuth } from "../auth/useAuth";
-
-// export default function ProtectedRoute() {
-//   const { user, loading } = useAuth();
-
-//   if (loading) return <p>Loading...</p>;
-//   if (!user) return <Navigate to="/login" replace />;
-
-//   return <Outlet />;
-// }
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../auth/useAuth";
 
@@ -21,10 +10,8 @@ export default function ProtectedRoute({ adminOnly = false }: Props) {
 
   if (loading) return <p>Loading...</p>;
 
-  // belum login
   if (!user) return <Navigate to="/login" replace />;
 
-  // bukan admin tapi masuk halaman admin
   if (adminOnly && user.role !== "ADMIN") return <Navigate to="/list" replace />;
 
   return <Outlet />;
